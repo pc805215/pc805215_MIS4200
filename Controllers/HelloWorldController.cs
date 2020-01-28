d977ed842c4bce661da2ba29f9ccc753df532759
Parker Cantwell <pc805215@ohio.edu>
2020-01-27 23:13:04 -0500

    Adjusted the page to meet requirements of SD1

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pc805215_MIS4200.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
